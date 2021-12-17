7b667da4e6a6ceb7de1702f03e21c999bbf8be85
BetterCyber <78450870+bettercyberllc@users.noreply.github.com>
2021-12-17 16:06:50 -0600

    Update wazuh-agent-install.sh

--- wazuh-agent-install.sh
+++ wazuh-agent-install.sh
@@ -1,11 +1,16 @@
-!#/bin/bash
+touch ~/wazuh-agent-install.sh
+
+echo '!#/bin/bash
 curl -s https://packages.wazuh.com/key/GPG-KEY-WAZUH | apt-key add -
 echo "deb https://packages.wazuh.com/4.x/apt/ stable main" | tee -a /etc/apt/sources.list.d/wazuh.list
 apt-get update
 # Set Wazuh Manager IP address
-WAZUH_MANAGER="10.0.0.2" apt-get install wazuh-agent
+WAZUH_MANAGER="10.10.10.10" apt-get install wazuh-agent
 systemctl daemon-reload
 systemctl enable wazuh-agent
 systemctl start wazuh-agent
+exit' > ~/wazuh-agent-install.sh
 
-exit
+chmod +x ~/wazuh-agent-install.sh
+
+/bin/bash ~/wazuh-agent-install.sh
